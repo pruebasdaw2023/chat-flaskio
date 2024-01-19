97de2eaaf0c7d1a6d603b628cc960d0480fa805e
Luis García <pruebasdaw2021@gmail.com>
2024-01-19 09:51:04 +0100

    Clave puesta en archivo .env con python-dotenv

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret'
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 socketio = SocketIO(app)
 
 
